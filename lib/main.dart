46b8a5bd2f50f29742c7291fa8e89a13abc7cbe8
Jerico Magdamit <jerry.coh14@gmail.com>
2022-08-14 22:01:38 +1000

    Adding all the files into Github repository

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+// make sure to import these packages
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'dart:io';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ScanQrPage(),
+    );
+  }
+}
+
+class ScanQrPage extends StatefulWidget {
+  const ScanQrPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ScanQrPage();
+}
+
+class _ScanQrPage extends State<ScanQrPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // Variables needed for the QR Scanning
+  Barcode? result;
+  QRViewController? controller;
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            // This creates the small window where the user
+            // can view the QR code being scanned
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (result != null)
+                  // Upon scanning the QR code data will be
+                  //  printed in a text box below
+                  ? Text('QR Code Data: ${result!.code}')
+                  : const Text('Scan a code'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
